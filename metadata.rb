07bb1a47009ee91bf8815da2ffbdaa2de1a6630a
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-02-20 18:38:56 -0500

    update cookbook supermarket version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'zer0ne.io.x@gmail.com'
 license 'MIT'
 description 'Chef cookbook that installs and configures Systemd units: system components' \
             ' and services managed by the Linux systemd system/service manager.'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 14.0'
 %w[centos fedora redhat debian ubuntu].each do |os|
   supports os
