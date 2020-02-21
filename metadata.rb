68272249569d2bb87abd6b338d5971afd80dc6f8
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-02-20 19:09:26 -0500

    update cookbook version to fix existing tag quality metric (0.1.2 => 0.1.3)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'zer0ne.io.x@gmail.com'
 license 'MIT'
 description 'Chef cookbook that installs and configures Systemd units: system components' \
             ' and services managed by the Linux systemd system/service manager.'
-version '0.1.2'
+version '0.1.3'
 chef_version '>= 14.0'
 %w[centos fedora redhat debian ubuntu].each do |os|
   supports os
