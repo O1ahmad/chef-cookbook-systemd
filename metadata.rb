9e76c02388d04cec1af73307e7c7830db8027296
0xO1 <zer0ne.io.x@gmail.com>
2020-02-20 19:04:51 -0500

    update github repo metadata URLs

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'zer0ne.io.x@gmail.com'
 license 'MIT'
 description 'Chef cookbook that installs and configures Systemd units: system components' \
             ' and services managed by the Linux systemd system/service manager.'
-version '0.1.1'
+version '0.1.2'
 chef_version '>= 14.0'
 %w[centos fedora redhat debian ubuntu].each do |os|
   supports os
 end
-issues_url 'https://github.com/0x0I/chef-cookbook-template/issues'
-source_url 'https://github.com/0x0I/chef-cookbook-template'
+issues_url 'https://github.com/0x0I/chef-cookbook-systemd/issues'
+source_url 'https://github.com/0x0I/chef-cookbook-systemd'
